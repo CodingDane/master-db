bee0d0349a48bd9c9eee9be8b6ac35b64c55e530
alexander voler <avoeler@gmail.com>
2024-03-22 09:19:01 +0900

    mistake in tablename in migration file

--- db/migrate/20240322001354_change_poster_url_column.rb
+++ db/migrate/20240322001354_change_poster_url_column.rb
@@ -1,5 +1,5 @@
 class ChangePosterUrlColumn < ActiveRecord::Migration[7.1]
   def change
-    rename_column :wa_li_movie, :posterurl, :poster_url
+    rename_column :wa_li_movies, :posterurl, :poster_url
   end
 end
